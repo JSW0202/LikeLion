97c15789b96fb19be6c956326d83d1bd47ff2bb0
JSW0202 <jsu2929@naver.com>
2025-02-28 18:17:34 +0900

    add : study25.02.28

---
+++ ShootingGame/ShootingGame/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShootingGame
+{
+    //미사일 클래스
+    public class BULLET
+    {
+        public int x;
+        public int y;
+        public bool fire;
+    }
+    //플레이어 클래스
+    public class Player
+    {
+        [DllImport("msvcrt.dll")]
+        static extern int _getch();  //c언어 함수 가져옴
+
+        public int playerX;   //플레이어 X좌표
+        public int playerY;   //플레이어 Y좌표
+        public BULLET[] playerBullet = new BULLET[20];
+
+        public Player() //생성자
+        {
+            //플레이어 좌표위치 초기화
+            playerX = 0;
+            playerY = 12;
+
+            for (int i = 0; i < 20; i++) //총알 초기화
+            {
+                playerBullet[i] = new BULLET();
+                playerBullet[i].x = 0;
+                playerBullet[i].y = 0;
+                playerBullet[i].fire = false;
+            }
+
+
+        }
+
+        public void GameMain()
+        {
+            //키를 입력하는 부분
+            KeyControl();
+            //플레이얼를 그려준다.
+            PlayerDraw();
+        }
+
+        public void KeyControl()
+        {
+            int pressKey;  //정수형 변수선언 키값 받을거임 
+
+            if (Console.KeyAvailable) //키가 눌렸을때 true
+            {
+                pressKey = _getch(); //아스키값 왼쪽 오른쪽
+
+
+                switch (pressKey)
+                {
+                    case 72:  //위쪽방향 아스키코드 
+
+                        playerY--;
+                        if (playerY < 1)
+                            playerY = 1;
+                        break;
+                    case 75:
+                        //왼쪽 화살표키
+                        playerX--;
+                        if (playerX < 0)
+                            playerX = 0;
+                        break;
+                    case 77:
+                        //오른쪽
+                        playerX++;
+                        if (playerX > 75)
+                            playerX = 75;
+                        break;
+                    case 80: //아래
+                        playerY++;
+                        if (playerY > 23)
+                            playerY = 23;
+                        break;
+                    case 32: //스페이스바
+                        //총알 발사
+                        for (int i = 0; i < 20; i++)
+                        {
+                            //미사일이 false 발사가능
+                            if (playerBullet[i].fire == false)
+                            {
+                                playerBullet[i].fire = true;
+                                //플레이어 앞에서 미사일 쏘기 + 5
+                                playerBullet[i].x = playerX + 5;
+                                playerBullet[i].y = playerY + 1;
+                                //한발씩 쏘겠다.
+                                break;
+                            }
+                        }
+
+                        break;
+
+
+                }
+
+
+            }
+
+
+
+
+
+        }
+
+        //미사일 그리기
+        public void BulletDraw()
+        {
+
+        }
+
+        public void PlayerDraw()
+        {
+
+        }
+
+        //충돌처리
+        public void ClashEnemyAndBullet(Enemy enemy)
+        {
+
+        }
+
+    }
+
+    public class Enemy //적 클래스
+    {
+        public int enemyX;  //X좌표
+        public int enemyY;  //Y좌표
+
+        public Enemy()
+        {
+            //적 좌표 초기화
+            enemyX = 77;
+            enemyY = 12;
+
+        }
+
+        public void EnemyDraw()//적그리기
+        {
+
+        }
+
+        public void EnmeyMove()
+        {
+
+        }
+
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //플레이어 생성
+            Player player = new Player();
+            Enemy enemy = new Enemy(); //적생성
+
+            //유니티처럼 속도 프레임속도
+            int dwTime = Environment.TickCount;  // 1/1000 초가 흐른다.
+
+            while (true) //무한반복
+            {
+                //0.05초 지연
+                if (dwTime + 50 < Environment.TickCount)
+                {
+                    //현재시간 세팅
+                    dwTime = Environment.TickCount;
+                    Console.Clear();
+
+                    //플레이어
+                    player.GameMain();
+
+                    //총알
+                    player.BulletDraw();
+
+                    enemy.EnmeyMove();//적이동
+                    enemy.EnemyDraw();//적그리기
+
+
+                    //충돌처리
+                    player.ClashEnemyAndBullet(enemy);
+                }
+            }
+        }
+    }
+}
